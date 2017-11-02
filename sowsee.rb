4b9db0091f5437f060a705d5b533d36e32a01a35
yn1046 <wantedblood@2ch.hk>
2017-11-02 19:17:31 +0300

    Create sowsee.rb

---
+++ sowsee.rb
@@ -0,0 +1,25 @@
+Le govno d'Anale Souffle.
+
+Ingredients.
+72 g haricot beans
+117 eggs
+105 g lard
+32 cups oil
+115 zucchinis
+97 g dijon mustard
+105 potatoes
+
+Method.
+Put potatoes into the mixing bowl.
+Put dijon mustard into the mixing bowl.
+Put zucchinis into the mixing bowl.
+Put dijon mustard into the mixing bowl.
+Put zucchinis into the mixing bowl.
+Put oil into the mixing bowl.
+Put lard into the mixing bowl.
+Put eggs into the mixing bowl.
+Put haricot beans into the mixing bowl.
+Liquefy contents of the mixing bowl.
+Pour contents of the mixing bowl into the baking dish.
+
+Serves 1.
